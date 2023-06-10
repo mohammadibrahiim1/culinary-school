a98f0f49d5d56be7fcc163dbd7c1f88dcca841b7
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-06-11 03:10:10 +0600

    number component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
+import Number from "../../Components/Number/Number";
 
 const Home = () => {
   return (
     <div>
       <div>
         <Header />
+        <Number />
       </div>
     </div>
   );
